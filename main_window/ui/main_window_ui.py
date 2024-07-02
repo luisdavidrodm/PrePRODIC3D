96127d37e6f8de6b19d20cf724290d223e0e3f47
Luis D. Rodríguez <luisdavidrodm@gmail.com>
2024-07-02 12:07:34 -0400

    V1.0.1 Final

--- main_window/ui/main_window_ui.py
+++ main_window/ui/main_window_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'main_windowMBQkSU.ui'
+## Form generated from reading UI file 'main_windowjldpnJ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
